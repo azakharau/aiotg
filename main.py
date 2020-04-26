90b05a127cf126d6b7ff3c479f80972de3ccd55b
Alexey Zaharov <alexey.zaharov@lacit.net>
2020-04-26 12:25:46 +0300

    check delete_message

--- main.py
+++ main.py
@@ -9,22 +9,47 @@ import aiohttp
 
 import settings
 from tgapi import apimethods
-from tgapi.tgtypes import update
+from tgapi.tgtypes import update, base
+
 
 async def main():
+    path = f"{os.getcwd()}/test.json"
     async with aiohttp.TCPConnector(family=socket.AF_INET,
-                              ssl=False) as connector:
+                                    ssl=False) as connector:
         async with aiohttp.ClientSession(connector=connector) as session:
-            task = apimethods.get_updates(session,
-                                          settings.TELEGRAM_API_URL)
-            result = await asyncio.gather(task)
+            tasks = []
+            # task = apimethods.get_updates(session,
+            #                               settings.TELEGRAM_API_URL)
+            # tasks.append(task)
+            with open(path) as f:
+                data = json.load(f)
+            for dict_ in data:
+                if not dict_.get('message'):
+                    if dict_.get('edited_message'):
+                        task = apimethods.delete_message(session,
+                                                         settings.TELEGRAM_API_URL,
+                                                         dict_['edited_message'][
+                                                             'chat'][
+                                                             'id'],
+                                                         dict_['edited_message'][
+                                                             'message_id'])
+                        tasks.append(task)
+                        continue
+                    else:
+                        continue
+                task = apimethods.delete_message(session,
+                                                 settings.TELEGRAM_API_URL,
+                                                 dict_['message']['chat'][
+                                                     'id'],
+                                                 dict_['message'][
+                                                     'message_id'])
+                tasks.append(task)
+            result: list = await asyncio.gather(*tasks)
+            pprint(result)
+    if result:
+        with open(path, 'w') as f:
+            json.dump(*result, f, indent=2)
 
-            path = f"{os.getcwd()}/test.json"
-            if result:
-                with open(path, 'w') as f:
-                    json.dump(*result, f, indent=2)
-            u = [update.Update(**upd) for upd in result[0]]
-            pprint([dataclasses.asdict(a) for a in u])
 
 if __name__ == '__main__':
     asyncio.get_event_loop().run_until_complete(main())
