922324a9c393286b216f5478bef7d602cae2c9db
Alexey Zaharov <alexey.zaharov@lacit.net>
2020-04-29 00:46:25 +0300

    Add new execeptions

--- tgapi/exceptions.py
+++ tgapi/exceptions.py
@@ -1,2 +1,26 @@
-class FunctionNotAllowedInProduction(Exception):
+class FunctionNotAllowed(Exception):
+    ...
+
+
+class TelegramBaseException(Exception):
+    ...
+
+
+class TelegramAPIException(TelegramBaseException):
+    ...
+
+
+class TelegramDataObjectException(TelegramBaseException):
+    ...
+
+
+class HandlerException(TelegramBaseException):
+    ...
+
+
+class HandlerCloseException(TelegramBaseException):
+    ...
+
+
+class DispatcherException(TelegramBaseException):
     ...
