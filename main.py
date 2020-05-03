1afca41099e5a9780a2365811000f995a744ffc6
Alexey Zaharov <alexey.zaharov@lacit.net>
2020-05-04 00:56:19 +0300

    Add unpack to lists with single objects

--- main.py
+++ main.py
@@ -6,29 +6,27 @@ import uvloop
 from tgapi.bot.bot import Bot
 from tgapi.dispatcher.dispatcher import Dispatcher
 
-tasks = []
-path = f"{os.getcwd()}/test.json"
-
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 
 bot = Bot()
 
 dispatcher = Dispatcher(bot=bot)
 
+
 @dispatcher.message_handler(commands=['/hello'])
 async def t(message):
-    await bot.send_message(message[0].from_user['id'],
-                           f"Hello, {message[0].from_user['first_name']}")
+    await bot.send_message(message.from_user['id'],
+                           f"Hello, {message.from_user['first_name']}")
 
-@dispatcher.message_handler(lambda msg: msg.lower().startswith('p'))
+
+@dispatcher.message_handler(lambda msg: msg.lower().startswith('privet'))
 async def tt(message):
-    await bot.send_message(message[0].from_user['id'], "Hi")
+    await bot.send_message(message.from_user['id'], "Hi")
+
 
 @dispatcher.message_handler()
 async def ttt(message):
     await bot.send_message(424177117, "anon")
-
-
 
 
 if __name__ == '__main__':
